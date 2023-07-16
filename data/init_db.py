beb621ab3176c75f2374d60ad95913162b70f8dc
Léon Melein <leon@thesociallions.nl>
2023-07-16 22:17:08 +0200

    Dataset refresh

---
+++ data/init_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+db = sqlite3.connect("dataset.sqlite3")
+
+dbContext = db.cursor()
+dbContext.execute("CREATE TABLE locations(id, name, alternate_name, lat, long)")
+dbContext.execute("CREATE TABLE status(id, status, temperature, timestamp)")
+
+
+

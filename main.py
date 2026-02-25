e9d09d124c3f3428dfb2072fc9e0e93d459ba329
Léon Melein <leon@thesociallions.nl>
2026-02-25 17:23:07 +0100

    Re-enable all sources

--- main.py
+++ main.py
@@ -80,21 +80,21 @@ class Ingester():
 
 if __name__ == "__main__":
     regions = [
-        # Netherlands(),
-        # Belgium(),
-        # Austria(),
-        # Switzerland(),
-        # Luxembourg(),
-        # # Poland()
-        # Lithuania(),
-        # Sweden(),
-        # Denmark(),
-        # Czechia(),
-        # Slovakia(),
-        # Hungary(),
-        # UnitedKingdom(),
-        # Croatia(),
-        # Latvia()
+        Netherlands(),
+        Belgium(),
+        Austria(),
+        Switzerland(),
+        Luxembourg(),
+        # Poland()
+        Lithuania(),
+        Sweden(),
+        Denmark(),
+        Czechia(),
+        Slovakia(),
+        Hungary(),
+        UnitedKingdom(),
+        Croatia(),
+        Latvia(),
         Ireland()
     ]
     loader = Ingester(regions, generateEU=True)
